560bc297d52e2e9dc67ecb838611282500ee5827
TurtleStoffel <me@turtlestoffel.com>
2025-04-07 19:38:14 +0200

    Hacky parse file implementation

--- src/parse-file.test.ts
+++ src/parse-file.test.ts
@@ -1,5 +1,10 @@
 import { assertEquals } from "@std/assert/equals";
-import { parseChunks } from "./parse-file.ts";
+import {
+    getChildContent,
+    parseChunks,
+    parseNestedChunks,
+    stripPreContent,
+} from "./parse-file.ts";
 
 Deno.test(function chunksAreCorrectlyParsed() {
     const content = `# Title 1
@@ -9,10 +14,85 @@ This is some content.
 This is some more content.`;
 
     const result = parseChunks(content);
-    console.log(result);
     assertEquals(result, [
         "# Title 1",
         "## Subtitle 1\nThis is some content.",
         "## Subtitle 2\nThis is some more content.",
     ]);
+});
+
+Deno.test(function parseRootReturnsRootNode() {
+    const content = `# Title 1
+## Subtitle 1
+This is some content.
+## Subtitle 2
+This is some more content.`;
+
+    const result = parseNestedChunks(content, 0);
+    assertEquals(result.content, content);
+    assertEquals(result.children.length, 2);
+    assertEquals(result.depth, 0);
+
+    const firstChild = result.children[0];
+    assertEquals(
+        firstChild.content,
+        "## Subtitle 1\nThis is some content.",
+    );
+    assertEquals(firstChild.children.length, 0);
+    assertEquals(firstChild.depth, 1);
+
+    const secondChild = result.children[1];
+    assertEquals(
+        secondChild.content,
+        "## Subtitle 2\nThis is some more content.",
+    );
+    assertEquals(secondChild.children.length, 0);
+    assertEquals(secondChild.depth, 1);
+});
+
+Deno.test(function getChildContentReturnsCorrectChildContent() {
+    const content = `# Title 1
+## Subtitle 1
+This is some content.
+## Subtitle 2
+This is some more content.`;
+
+    const result = getChildContent(content);
+
+    assertEquals(
+        result,
+        "## Subtitle 1\nThis is some content.\n## Subtitle 2\nThis is some more content.",
+    );
+});
+
+Deno.test(function getChildContentReturnsCorrectChildContent() {
+    const content = `some pre-content content
+# Title 1
+## Subtitle 1
+This is some content.
+## Subtitle 2
+This is some more content.`;
+
+    const result = getChildContent(content);
+
+    assertEquals(
+        result,
+        "## Subtitle 1\nThis is some content.\n## Subtitle 2\nThis is some more content.",
+    );
+});
+
+Deno.test(function stripPreContentRemovesContentBeforeFirstTitle() {
+    const content = `some pre-content content
+# Title 1
+## Subtitle 1
+This is some content.
+## Subtitle 2
+This is some more content.`;
+
+    const result = stripPreContent(content);
+
+    assertEquals(
+        result,
+        "## Subtitle 1\nThis is some content.\n## Subtitle 2\nThis is some more content.",
+    );
 });
--- src/parse-file.ts
+++ src/parse-file.ts
@@ -9,7 +9,6 @@ export function parseChunks(content: string): string[] {
     let chunkStart = 0;
     for (let i = 0; i < fileContentLines.length; i++) {
         if (fileContentLines[i].startsWith("#")) {
-            console.log(fileContentLines[i]);
             if (i > chunkStart) {
                 chunks.push(fileContentLines.slice(chunkStart, i).join("\n"));
             }
@@ -23,4 +22,96 @@ export function parseChunks(content: string): string[] {
     }
 
     return chunks;
+}
+
+export function parseChunksByDelimiter(
+    content: string,
+    delimiter: string,
+): string[] {
+    const fileContentLines = content.split("\n");
+
+    // Split the file content into chunks split by lines starting with a hashtag
+    const chunks = [];
+    let chunkStart = 0;
+    for (let i = 0; i < fileContentLines.length; i++) {
+        if (fileContentLines[i].startsWith(delimiter)) {
+            if (i > chunkStart) {
+                chunks.push(fileContentLines.slice(chunkStart, i).join("\n"));
+            }
+            chunkStart = i;
+        }
+    }
+
+    // Add the last chunk
+    if (chunkStart < fileContentLines.length) {
+        chunks.push(fileContentLines.slice(chunkStart).join("\n"));
+    }
+
+    return chunks;
+}
+
+export interface Node {
+    content: string;
+    children: Node[];
+    depth: number;
+}
+
+export function parseRoot(content: string): Node {
+    const strippedContent = stripPreContent(content);
+    const root = parseNestedChunks(strippedContent, 0);
+
+    return root;
+}
+
+export function stripPreContent(content: string): string {
+    const lines = content.split("\n");
+
+    let result = "";
+
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i].startsWith("# ")) {
+            result = lines.slice(i + 1).join("\n");
+        }
+    }
+
+    return result;
+}
+
+export function parseNestedChunks(content: string, depth: number): Node {
+    const childContent = getChildContent(content);
+
+    if (childContent === "") {
+        return {
+            content,
+            children: [],
+            depth: depth,
+        };
+    }
+
+    const delimiter = "#".repeat(depth + 2) + " ";
+    const chunks = parseChunksByDelimiter(childContent, delimiter);
+
+    const children = chunks.map((chunk) => parseNestedChunks(chunk, depth + 1));
+
+    return {
+        content,
+        children: children,
+        depth: depth,
+    };
+}
+
+export function getChildContent(content: string): string {
+    const lines = content.split("\n");
+
+    const childContentDelimiter = "# ";
+    let childContent = "";
+
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i].startsWith(childContentDelimiter)) {
+            childContent = lines.slice(i + 1).join("\n");
+            break;
+        }
+    }
+
+    return childContent;
 }
