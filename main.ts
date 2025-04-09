7e26ea97451c15e55c854c69bdfad08276e03fd4
TurtleStoffel <me@turtlestoffel.com>
2025-04-09 05:59:43 +0200

    Sequentially query hierarchical chunks

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { parseChunks } from "./src/parse-file.ts";
+import { createContentList, parseRoot } from "./src/parse-hierarchical.ts";
 
 const stylePrompt = `You are a helpful assistant that helps me write better.
 You will give me a score from 1 to 10 on how good the text is. You will also give me some suggestions on how to improve the text.
@@ -21,11 +22,40 @@ const filename =
     "/Users/stefan.wauters/coding/personal/blog/content/private/Writing - Private Notes.md";
 
 const fileContent = await Deno.readTextFile(filename);
-const chunks = parseChunks(fileContent);
+
+const root = parseRoot(fileContent);
+const chunks = createContentList(root);
+// const chunks = parseChunks(fileContent);
 
 // Add the prompt to each chunk
 const chunksWithPrompt = chunks.map((chunk) => `${stylePrompt}\n\n${chunk}`);
 
+for (let i = 0; i < chunksWithPrompt.length; i++) {
+    const chunk = chunksWithPrompt[i];
+    const response = await fetch("http://localhost:11434/api/generate", {
+        method: "POST",
+        body: JSON.stringify({
+            model,
+            prompt: chunk,
+            stream: false,
+        }),
+    });
+
+    const content = await response.text();
+    const parsedContent = JSON.parse(content);
+
+    console.log("---CHUNK RESULT---");
+    console.log("---ORIGINAL TEXT---");
+    console.log(chunk);
+
+    console.log("---RESPONSE---");
+    console.log(parsedContent.response);
+    console.log("---CHUNK RESULT END---\n\n\n");
+
+    alert("Press enter to continue to next chunk...");
+}
+
+/*
 chunksWithPrompt.forEach(async (chunk) => {
     const response = await fetch("http://localhost:11434/api/generate", {
         method: "POST",
@@ -47,5 +77,6 @@ chunksWithPrompt.forEach(async (chunk) => {
     console.log(parsedContent.response);
     console.log("---CHUNK RESULT END---");
 });
+*/
 
 console.log(fileContent);
